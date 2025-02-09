b0c315bc17ca7ec10c639f1393d88ad0d2b74abd
Serik <serik.askaroff@gamil.com>
2025-02-09 20:20:55 +0300

    тестирую манифесты

--- vite.config.ts
+++ vite.config.ts
@@ -20,13 +20,8 @@ export default defineConfig({
 				theme_color: '#ffffff',
 				icons: [
 					{
-						src: 'pwa-192x192.png',
+						src: 'user.png',
 						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
 						type: 'image/png'
 					}
 				]
